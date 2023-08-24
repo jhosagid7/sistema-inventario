cce7643d36a5022cf9798f53b929143af8274870
Jhonny Pirela <jhosagid7@gmail.com>
2023-08-24 08:54:16 -0400

    add model and migrations initial

---
+++ app/Models/OutputDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OutputDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['quantity', 'comment', 'output_id', 'refaccion_id'];
+}
